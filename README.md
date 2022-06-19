3f151e96415a5f02edbd2ab50739bb838445a65b
jinbekim <jinbekim@student.42seoul.kr>
2022-06-19 11:41:50 +0900

    Add: dotenv

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# session
-session과 passport 공부
+# Session authorization
+## _목표_
+* 간단한 모듈부터 차례차례 사용하면서 각각의 역할 알기!
+
+## _한 것_
+- [ ] dotenv 설치
+- [ ] express 설치
+- [ ] express-session 설치
+- [ ] passport 설치
---
+++ app.js
@@ -0,0 +1,8 @@
+/**
+ * Dotenv is a zero-dependency module
+ * that loads environment variables
+ * from a .env file into process.env.
+ * https://www.npmjs.com/package/dotenv
+ */
+require('dotenv').config();
+console.log(process.env);
