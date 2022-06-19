188c6d76c7fafa986807bc09d6695de0a9feb483
jinbekim <jinbekim@student.42seoul.kr>
2022-06-19 11:51:35 +0900

    Add: express module & running minimum server

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 * 간단한 모듈부터 차례차례 사용하면서 각각의 역할 알기!
 
 ## _한 것_
-- [ ] dotenv 설치
-- [ ] express 설치
+- [x] dotenv 설치
+- [x] express 설치
 - [ ] express-session 설치
 - [ ] passport 설치
--- app.js
+++ app.js
@@ -5,4 +5,14 @@
  * https://www.npmjs.com/package/dotenv
  */
 require('dotenv').config();
-console.log(process.env);
+
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  // deprecated `res.send`
+  // res.send(200);
+  res.sendStatus(200);
+});
+
+app.listen(process.env.PORT || 3000);
